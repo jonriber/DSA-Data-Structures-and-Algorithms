81e9d8f12a1d384203243a64dd7f7e8c0239b807
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-28 22:40:26 +0100

    first approach solution

--- ALGORITHMS/TWO POINTER TECHNIQUE/167. Two Sum II - Input Array Is Sorted/two_sum_2.py
+++ ALGORITHMS/TWO POINTER TECHNIQUE/167. Two Sum II - Input Array Is Sorted/two_sum_2.py
@@ -1,7 +1,22 @@
 def two_sum(numbers, target):
-  pass
+  left, right = 0, len(numbers) - 1
+  
+  while left < right:
+    current_sum = numbers[left] + numbers[right]
+    if current_sum == target:
+      return [left +1, right + 1]
+    elif current_sum < target:
+      left += 1
+    else:
+      right -= 1
+  return []
 
 
 if __name__ == '__main__':
   # Trials
   result = two_sum([2,7,11,15], 9)
+  print(result)
+  result = two_sum([2,3,4], 6)
+  print(result)
+  result = two_sum([-1,0], -1)
+  print(result)
