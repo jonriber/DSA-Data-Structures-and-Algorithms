5fde99e4a68e5406a9cf626608cba1733191e3b7
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-29 13:29:58 +0100

    trying to add the - character

--- ALGORITHMS/TWO POINTER TECHNIQUE/26. Remove Duplicates from Sorted Array/remove_duplicates.py
+++ ALGORITHMS/TWO POINTER TECHNIQUE/26. Remove Duplicates from Sorted Array/remove_duplicates.py
@@ -1,17 +1,23 @@
 
 # integer array sorted in non-decreasing order
 def remove_duplicates(nums):
-  [i, j] = [0, 1]
+  [i, j,exclusive_number_counter] = [0, 1, 0]
   while j < len(nums):
     print("j is lower than len(nums)")
     print(f"i: {i}, j: {j}")
     if nums[i] != nums[j]:
+      exclusive_number_counter += 1
       print("nums[i] is not equal to nums[j]")
       i += 1
       nums[i] = nums[j]
       print(f"i: {i}, j: {j}")
+    else:
+      print("nums[i] is equal to nums[j]")
+      nums[j] = "-"
     j += 1
     print("J now is incremented by 1",j)
+  return exclusive_number_counter, nums
 
 if __name__ == "__main__":
-  remove_duplicates([1, 1, 2, 2, 3]) # 2
+  result = remove_duplicates([1, 1, 2, 2, 3])
+  print(result)
