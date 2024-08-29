1a18c290ee5d60a12acf8ae75782690c64226fbb
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2024-08-29 13:00:30 +0100

    investigating the code and logic

--- ALGORITHMS/TWO POINTER TECHNIQUE/26. Remove Duplicates from Sorted Array/remove_duplicates.py
+++ ALGORITHMS/TWO POINTER TECHNIQUE/26. Remove Duplicates from Sorted Array/remove_duplicates.py
@@ -1,7 +1,17 @@
 
-
+# integer array sorted in non-decreasing order
 def remove_duplicates(nums):
-  pass
+  [i, j] = [0, 1]
+  while j < len(nums):
+    print("j is lower than len(nums)")
+    print(f"i: {i}, j: {j}")
+    if nums[i] != nums[j]:
+      print("nums[i] is not equal to nums[j]")
+      i += 1
+      nums[i] = nums[j]
+      print(f"i: {i}, j: {j}")
+    j += 1
+    print("J now is incremented by 1",j)
 
 if __name__ == "__main__":
-  pass
+  remove_duplicates([1, 1, 2, 2, 3]) # 2
