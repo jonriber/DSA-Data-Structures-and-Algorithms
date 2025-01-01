2a55882f87c252347923a050c968e0aed9baf8bf
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-01-01 21:01:27 +0000

    [JAN,1] - sliding window starter problem

---
+++ ALGORITHMS/SLIDING WINDOW/starter/problem_description.md
@@ -0,0 +1,14 @@
+# Starter problem
+
+Given an array of positive integers and a number K, find the maximum sum of any contiguous subarray of size K.
+
+## example
+
+Input:
+
+```arr = [2, 1, 5, 1, 3, 2]
+K = 3
+```
+
+output:
+`9`
---
+++ ALGORITHMS/SLIDING WINDOW/starter/starter_problem.py
@@ -0,0 +1,11 @@
+def max_sum_subarray(arr, K):
+    pass
+
+
+if (__name__ == '__main__'):
+    # pass
+    # Write
+    
+    # Test cases
+  print(max_sum_subarray([2, 1, 5, 1, 3, 2], 3))  # Expected: 9
+  print(max_sum_subarray([2, 3, 4, 1, 5], 2))     # Expected: 7
