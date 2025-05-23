4a18dd52bb4917fde8b48d7d7958ba5d78018cd5
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-05-23 19:25:24 +0100

    [MAY,23] - factory Exercise 1 example

--- Design Patterns/Creational/factory/exercise_1_factory.py
+++ Design Patterns/Creational/factory/exercise_1_factory.py
@@ -9,8 +9,37 @@
 # 2. Subclasses: Circle, Square, Triangle.
 # 3. The factory should return the correct subclass based on input.
 
+class Shape:
+  def draw(self):
+    pass
+  
+class Circle(Shape):
+  def draw(self):
+    return "Drawing a Circle"
+  
+class Square(Shape):
+  def draw(self):
+    return "Drawing a Square"
+  
+class Triangle(Shape):
+  def draw(self):
+    return "Drawing a Triangle"
 
-
+class ShapeFactory:
+  def create_shape(self, shape_type:str) -> Shape:
+    if shape_type == "Circle":
+      return Circle()
+    elif shape_type == "Square":
+      return Square()
+    elif shape_type == "Triangle":
+      return Triangle()
+    else:
+      raise ValueError("Unknown shape type")
 
 if __name__ == "__main__":
-  pass
+  print("Factory Pattern Example")
+  factory = ShapeFactory()
+  shapes = ["Circle", "Square", "Triangle"]
+  for shape in shapes:
+    shape_instance = factory.create_shape(shape)
+    print(shape_instance.draw())
