0c79e6afe991342105f476157d883908be6b3744
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-05-23 19:37:01 +0100

    [MAY,23] - Singleton Pytest file

---
+++ Design Patterns/Creational/singleton/test_singleton_pattern.py
@@ -0,0 +1,20 @@
+import pytest
+from ..singleton.singleton_pattern import Singleton
+
+class TestSingletonClass1:
+  def test_class1_is_singleton(self):
+    class1 = Singleton()
+    class2 = Singleton()
+    assert class1 is class2
+
+class TestSingletonClass2:
+  def test_class2_is_singleton(self):
+    class2 = Singleton()
+    class3 = Singleton()
+    assert class2 is class3
+
+class TestSingletonClass3:
+  def test_class3_is_singleton(self):
+    class1 = Singleton()
+    class3 = Singleton()
+    assert class1 is class3
