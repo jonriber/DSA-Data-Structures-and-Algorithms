3563e8c0670287e10617086a6c8fa41406bc51bb
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-05-23 19:16:51 +0100

    [MAY,23] - Factory practical example & first exercise

---
+++ Design Patterns/Creational/factory/exercise_1_factory.py
@@ -0,0 +1,16 @@
+#Factory Simple example
+
+# Description:
+# Implement a simple ShapeFactory that creates shapes: Circle, Square, and Triangle. Each shape 
+# must have a .draw() method that returns a string like "Drawing a Circle"
+
+#requirements:
+# 1. Use a base class Shape.
+# 2. Subclasses: Circle, Square, Triangle.
+# 3. The factory should return the correct subclass based on input.
+
+
+
+
+if __name__ == "__main__":
+  pass
