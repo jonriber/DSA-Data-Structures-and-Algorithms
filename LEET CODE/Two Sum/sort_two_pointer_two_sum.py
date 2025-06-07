07ac80b0f760e721ce09579cbe3aa0b0e5085b3e
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-07 11:54:09 +0100

    [JUN,7] - two sum with 2 pointers

---
+++ LEET CODE/Two Sum/sort_two_pointer_two_sum.py
@@ -0,0 +1,14 @@
+
+def two_sum(nums, target):
+  """_summary_
+
+  Args:
+      nums (_type_): _description_
+      target (_type_): _description_
+  """
+
+  
+
+if __name__ == "__main__":
+  pass
+
