b6081dda8640d3234ad3dcb453b78319aa6ce93f
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-07 11:24:11 +0100

    [JUN,07] - two_summ leet code

---
+++ LEET CODE/Two Sum/hash_map_two_sum.py
@@ -0,0 +1,15 @@
+
+def two_sum_solution(nums, target):
+  """
+  Find two indices in nums such that the numbers at those indices add up to target.
+
+  :param nums: List[int] - List of integers
+  :param target: int - Target sum
+  :return: List[int] - Indices of the two numbers that add up to target
+  """
+  hasher = {} 
+  
+  
+
+if __name__ == "__main__":
+  pass
