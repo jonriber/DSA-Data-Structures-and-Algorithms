a70e0d76b24c4cce12d2aeb69286a233b296c94f
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-09 00:11:49 +0100

    [JUN,9] - group anagrams brute force solution using dictionary and sorted arrays

--- LEET CODE/Group Anagrams/group_anagrams_brute_force.py
+++ LEET CODE/Group Anagrams/group_anagrams_brute_force.py
@@ -1,7 +1,30 @@
+from collections import defaultdict
+
 
 def group_anagrams(strs):
+  """_summary_
 
-  pass
+  Args:
+      strs (_type_): _description_
+  """
+  anagrams = defaultdict(list)
+
+  for word in strs:
+    print(f"Processing word: {word}")
+
+    sorted_word = tuple(sorted(word))
+
+    print(f"Sorted word: {sorted_word}")
+
+    anagrams[sorted_word].append(word)
+
+    print(f"Anagrams grouped: {anagrams}")
+
+  # return list(anagrams.keys())
+  return list(anagrams.values())
+  
 
 if __name__ == "__main__":
-  pass
+  strs = ["eat","tea","tan","ate","nat","bat"]
+  result = group_anagrams(strs)
+  print(f"Grouped anagrams: {result}")
