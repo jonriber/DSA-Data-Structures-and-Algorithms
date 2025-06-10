08f791f382c110bae7f4228cc89fe2630c343cce
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-10 18:17:07 +0100

    [JUN,10] - breaking downs anagram solution with a dictionary and lists inside

--- LEET CODE/Group Anagrams/group_anagrams_brute_force.py
+++ LEET CODE/Group Anagrams/group_anagrams_brute_force.py
@@ -23,8 +23,39 @@ def group_anagrams(strs):
   # return list(anagrams.keys())
   return list(anagrams.values())
   
+def group_anagram_no_cp(words):
+
+  anagrams = defaultdict(list)
+
+  print(f"Anagrams: {anagrams}")
+
+  for word in words:
+
+    print("The word is: ", word)
+
+    sorted_word = sorted(word)
+    print(f"Sorted word: {sorted_word}")
+
+    tuple_sorted_word = tuple(sorted_word)
+    print(f"Tuple sorted word: {tuple_sorted_word}")
+
+    anagrams[tuple_sorted_word].append(word)
+    print(f"Anagrams grouped: {anagrams}")
+
+  print(f"Anagrams grouped: {anagrams}")
+
+  print(f"Anagrams grouped values: {anagrams.values()}")
+
+  print(f"Anagrams grouped keys: {anagrams.keys()}")
+
+  print(f"Anagrams grouped items: {anagrams.items()}")
+
+  print(f"Anagrams grouped as list: {list(anagrams.values())}")
+
+  return list(anagrams.values())
 
 if __name__ == "__main__":
   strs = ["eat","tea","tan","ate","nat","bat"]
-  result = group_anagrams(strs)
-  print(f"Grouped anagrams: {result}")
+  # result = group_anagrams(strs)
+  result2 = group_anagram_no_cp(strs)
+  # print(f"Grouped anagrams: {result}")
