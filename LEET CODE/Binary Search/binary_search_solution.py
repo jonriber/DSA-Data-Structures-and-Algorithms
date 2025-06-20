866041272a6f874f249b2aab1089bd4ee7ce9dde
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-20 14:43:17 +0100

    [JUN,20] - Binary search solution

--- LEET CODE/Binary Search/binary_search_solution.py
+++ LEET CODE/Binary Search/binary_search_solution.py
@@ -1,6 +1,17 @@
 def binary_search(nums, target):
-  pass
-
+  left_pointer, right_pointer = 0, len(nums) -1
+  
+  while left_pointer <= right_pointer:
+    mid = (left_pointer + right_pointer) // 2
+    if nums[mid] == target:
+      return mid
+    elif nums[mid] < target:
+      left_pointer = mid +1
+    else:
+      right_pointer = mid -1
+      
+  return -1
+    
 if __name__ == "__main__":
   nums1= [-1,0,3,5,9,12]
   target1 = 9
