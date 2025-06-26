5d8540c6510035fa1254bac892ed152cb27f06d1
jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-26 22:59:42 +0100

    [JUN,26] - valid parenthesis - leetcode

---
+++ LEET CODE/Valid Parentheses/valid_parenthesis_solution.py
@@ -0,0 +1,9 @@
+
+def valid_parenthesis(s:str) -> bool:
+  """
+    @param s: str - input string containing only '(', ')', '{', '}', '[' and ']'
+    @return: bool - True if the input string is valid, False otherwise
+  """
+  pass
+if __name__ == "__main__":
+  pass
