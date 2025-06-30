60066d04481c902092123c94b9d5a7f22cca47b1
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-30 09:42:45 +0100

    [JUN,30] - house robber dynamic programming

---
+++ LEET CODE/House Robber/house_robber_solution.py
@@ -0,0 +1,13 @@
+def rob_house(nums: list[int]) -> int:
+  pass
+    
+
+if __name__ == '__main__':
+  test1 = [1, 2, 3, 1]
+  test2 = [2, 7, 9, 3, 1]
+  
+  result1 = rob_house(test1)
+  result2 = rob_house(test2)
+  
+  print(f"Test 1 Result: {result1}")  # Expected output: 4
+  print(f"Test 2 Result: {result2}")  # Expected output: 12
