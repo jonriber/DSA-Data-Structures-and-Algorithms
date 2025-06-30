107b1bf3ba4fee8bc03d067dc520d01fb3785c63
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-06-30 10:50:41 +0100

    [JUN,30] - house robber solution

--- LEET CODE/House Robber/house_robber_solution.py
+++ LEET CODE/House Robber/house_robber_solution.py
@@ -1,5 +1,18 @@
 def rob_house(nums: list[int]) -> int:
-  pass
+  if not nums:
+    return 0
+  if len(nums) <= 2:
+    return max(nums)
+  
+  dp = [0]* len(nums)
+  dp[0] = nums[0]
+  dp[1] = max(nums[0], nums[1])
+  
+  for i in range(2, len(nums)):
+    dp[i] = max(nums[i]+ dp[i-2], dp[i-1])
+    
+  return dp[-1]
+    
     
 
 if __name__ == '__main__':
