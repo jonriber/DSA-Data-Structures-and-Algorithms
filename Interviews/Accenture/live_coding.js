043650013072e8e4c43121203f80cff2961e4e59
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-07-03 09:23:33 +0100

    [JUL,3] - accenture live coding solution without reversing array

--- Interviews/Accenture/live_coding.js
+++ Interviews/Accenture/live_coding.js
@@ -1,15 +1,22 @@
 
 function createObject(stringToParse, valueToAssign) {
-  //to imlement
-  // let finalObject = {};
-  // let new_array = stringToParse.split(".");
-  // console.log(new_array);
-  // for (let i in new_array) {
-  //   console.log("char:", new_array[i]);
-  //   let char = new_array[i];
-  //   finalObject[char] = {};
-  // }
-  // return finalObject;
+  const keys = stringToParse.split('.');
+  const result = {};
+  let current = result;
+  console.log("Keys:", keys);
+  console.log(`"Initial Result Object: ${JSON.stringify(result)}`);
+  for (let i=0; i < keys.length; i++){
+    const key = keys[i];
+    console.log(`Current Key: ${key}`);
+    if (i == keys.length -1){
+      current[key] = valueToAssign;
+    } else {
+      current[key] = {};
+      current = current[key];
+    }
+  }
+  console.log(`Final Result Object: ${JSON.stringify(result)}`);
+  return result;
 }
 console.log(createObject("a.b.c.d.e", 2));
 
