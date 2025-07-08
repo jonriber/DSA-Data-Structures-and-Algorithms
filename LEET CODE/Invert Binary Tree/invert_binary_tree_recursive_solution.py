a847cc46d53e6703bd17a534a0ead0abc845881d
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2025-07-08 08:52:49 +0100

    [JUL,8] - root usage with recursion

--- LEET CODE/Invert Binary Tree/invert_binary_tree_recursive_solution.py
+++ LEET CODE/Invert Binary Tree/invert_binary_tree_recursive_solution.py
@@ -12,4 +12,19 @@ def invert_binary_tree(root: TreeNode) -> TreeNode:
   return root
 
 if __name__ == "__main__":
-  pass
+  root1 = TreeNode(4)
+  root1.left = TreeNode(2)
+  root1.right = TreeNode(7)
+  root1.left.left = TreeNode(1)
+  root1.left.right = TreeNode(3)
+  root1.right.left = TreeNode(6)
+  root1.right.right = TreeNode(9)
+  inverted_tree1 = invert_binary_tree(root1)
+  print(inverted_tree1.val)  # Output: 4
+  print(inverted_tree1.left.val)  # Output: 7
+  print(inverted_tree1.right.val)  # Output: 2
+  print(inverted_tree1.left.left.val)  # Output: 9
+  print(inverted_tree1.left.right.val)  # Output: 6
+  print(inverted_tree1.right.left.val)  # Output: 3
+  print(inverted_tree1.right.right.val)  # Output: 1
+  
